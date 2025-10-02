25a66528b4f09fdd8cbf9a406daeb63193ba3ecd
Chris Gonzalez <christogonz92@gmail.com>
2025-10-02 12:23:19 +0200

    Initial Commit

---
+++ TrackYourSets/TrackYourSetsApp.swift
@@ -0,0 +1,17 @@
+//
+//  TrackYourSetsApp.swift
+//  TrackYourSets
+//
+//  Created by Christopher Gonzalez on 2025-10-02.
+//
+
+import SwiftUI
+
+@main
+struct TrackYourSetsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
